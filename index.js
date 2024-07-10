81f846d403fe32dadd7e8237098e7a93593f0644
MiTange <tmrevo210@yahoo.co.jp>
2024-07-10 23:39:02 +0900

    add update, delete

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const express = require("express");
 const app = express();
 app.use(express.urlencoded({ extended: true}));
+const activities = require("./activities.json");
 
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
@@ -11,6 +12,21 @@ app.post("/autumn", function (req, res) {
     fs.writeFile(__dirname + "/data.txt", req.body.activity, function () {
         res.send("投稿完了");
     });
+});
+
+app.post("/update", function(req, res) {
+    const activity = req.body.updateActivity;
+    if (!activity) {
+        res.status(400).send("活動が未定義です");
+    } else {
+        activities[0].activity = activity;
+        res.send(activities);
+    }
+});
+
+app.post("/delete", function(req, res) {
+    activities.splice(req.body.number, 1);
+    res.send(activities);
 });
 
 const port = 5000;
